574f3d8ff734abcd38bea8a6638f23a42913db40
zhangxiuhua <zhangxiuhua@xiaomi.com>
2022-07-03 19:17:55 +0800

    feat: add 112 111

---
+++ test/hasPathSum_112.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ * 
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。如果存在，返回 true ；否则，返回 false 。
+
+叶子节点 是指没有子节点的节点。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+import { TreeNode } from "./binaryTree.js";
+
+let node3 = new TreeNode(3);
+let node9 = new TreeNode(9);
+let node20 = new TreeNode(20);
+let node15 = new TreeNode(15);
+let node7 = new TreeNode(7);
+node3.left = node9;
+node3.right = node20;
+node20.left = node15;
+node20.right = node7;
+/****
+ *       3
+ *    9    20
+ *       15   7
+ */
+
+var hasPathSum = function (root, targetSum) {
+  let flag = false;
+  let traverse = function (root, sum) {
+    if (flag) return;//已找到后续就不要在执行了
+    if (root === null) return;
+    if (!root.left && !root.right) {
+      // 叶子节点
+      if (sum + root.val === targetSum) {
+        flag = true;
+        return;
+      }
+    }
+    if (root.left) {
+      traverse(root.left, sum + root.val);
+    }
+    if (root.right) {
+      traverse(root.right, sum + root.val);
+    }
+  };
+  traverse(root, 0);
+  return flag;
+};
+console.log("hasPathSum");
+console.log(hasPathSum(node3, 12));
---
+++ test/minDepth_111.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ * 给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+ */
+
+import { TreeNode } from "./binaryTree.js";
+
+let node3 = new TreeNode(3);
+let node9 = new TreeNode(9);
+let node20 = new TreeNode(20);
+let node15 = new TreeNode(15);
+let node7 = new TreeNode(7);
+node3.right = node9;
+node9.right = node20;
+node20.right = node15;
+node15.right = node7;
+let min = Number.MAX_VALUE;
+let traverse = (root, depth) => {
+  if (!root) return;
+  if (!root.left && !root.right) {
+    min = Math.min(min, depth);
+  }
+  traverse(root.left, depth + 1);
+  traverse(root.right, depth + 1);
+};
+var minDepth = function (root) {
+  if (!root) return 0;
+  traverse(root, 1);
+  return min;
+};
+// console.log("minDepth");
+// console.log(minDepth(node3));
