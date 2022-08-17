2929dd8bd870625a11037b93ba4a0e8e7cf8156e
zhangxiuhua <zhangxiuhua@xiaomi.com>
2022-08-17 23:10:07 +0800

    datastructure day 10

---
+++ dataStructure/2.两数相加.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=javascript
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let root = new ListNode();
+  let p = root;
+  let plus = 0;
+  while (l1 || l2) {
+    let val1 = l1 ? l1.val : 0;
+    let val2 = l2 ? l2.val : 0;
+    let sum = val1 + val2 + plus;
+    let node = new ListNode(sum % 10);
+    p.next = node;
+    plus = parseInt(sum / 10);
+    p = p.next;
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  if (plus > 0) {
+    let node = new ListNode(plus);
+    p.next = node;
+  }
+
+  return root.next;
+};
+// @lc code=end
